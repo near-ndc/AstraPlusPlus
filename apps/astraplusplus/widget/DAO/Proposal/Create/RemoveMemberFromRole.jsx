57fca9e43a1e550d3ea7e02e5d14a92a669ce3fd
Megha-Dev-19 <meghadev1901@gmail.com>
2024-01-30 00:22:29 +0530

    fix proposal bond

--- apps/astraplusplus/widget/DAO/Proposal/Create/RemoveMemberFromRole.jsx
+++ apps/astraplusplus/widget/DAO/Proposal/Create/RemoveMemberFromRole.jsx
@@ -1,158 +1,149 @@
 const accountId = props.accountId ?? context.accountId;
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
 const onClose = props.onClose;
+const policy = props.daoPolicy;
 
 if (!accountId) {
-    return "Please connect your NEAR wallet :)";
+  return "Please connect your NEAR wallet :)";
 }
 
 State.init({
-    member_id: state.member_id,
-    role: state.role,
-    error: undefined,
-    rolesOptions: []
+  member_id: state.member_id,
+  role: state.role,
+  error: undefined,
+  rolesOptions: []
 });
 
 function isNearAddress(address) {
-    const ACCOUNT_ID_REGEX =
-        /^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/;
-    return (
-        address.length >= 2 &&
-        address.length <= 64 &&
-        ACCOUNT_ID_REGEX.test(address)
-    );
+  const ACCOUNT_ID_REGEX =
+    /^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/;
+  return (
+    address.length >= 2 &&
+    address.length <= 64 &&
+    ACCOUNT_ID_REGEX.test(address)
+  );
+}
+
+if (policy === null) {
+  return <></>;
 }
 
 const processPolicy = (policy) => {
-    const roles = {};
-    const options = [];
-    policy.roles.forEach((role) => {
-        if (role.kind.Group) {
-            if (!roles[role.name]) {
-                roles[role.name] = role;
-                options.push({ text: role.name, value: role.name });
-            }
-        }
-    });
-    State.update({ rolesOptions: options });
-    return roles;
+  const roles = {};
+  const options = [];
+  policy.roles.forEach((role) => {
+    if (role.kind.Group) {
+      if (!roles[role.name]) {
+        roles[role.name] = role;
+        options.push({ text: role.name, value: role.name });
+      }
+    }
+  });
+  State.update({ rolesOptions: options });
+  return roles;
 };
 
-const allowedRoles = useCache(
-    () =>
-        Near.asyncView(daoId, "get_policy").then((policy) =>
-            processPolicy(policy)
-        ),
-    daoId + "-remove-member-proposal",
-    { subscribe: false }
-);
-
-if (allowedRoles === null) {
-    return <></>;
-}
+const allowedRoles = processPolicy(policy);
 
 const handleProposal = () => {
-    if (
-        !state.member_id ||
-        state.member_id === "" ||
-        !isNearAddress(state.member_id)
-    ) {
-        State.update({
-            error: "Please enter a valid member ID"
-        });
-        return;
-    }
-    if (!state.role || state.role === "") {
-        State.update({
-            error: "Please enter a valid role"
-        });
-        return;
-    }
+  if (
+    !state.member_id ||
+    state.member_id === "" ||
+    !isNearAddress(state.member_id)
+  ) {
+    State.update({
+      error: "Please enter a valid member ID"
+    });
+    return;
+  }
+  if (!state.role || state.role === "") {
+    State.update({
+      error: "Please enter a valid role"
+    });
+    return;
+  }
 
-    const gas = 200000000000000;
-    const deposit = 100000000000000000000000;
-    Near.call([
-        {
-            contractName: daoId,
-            methodName: "add_proposal",
-            args: {
-                proposal: {
-                    description: "Remove member",
-                    kind: {
-                        RemoveMemberFromRole: {
-                            member_id: state.member_id ?? accountId,
-                            role: state.role ?? "council"
-                        }
-                    }
-                }
-            },
-            gas: gas,
-            deposit: deposit
+  const gas = 200000000000000;
+  const deposit = policy?.proposal_bond || 100000000000000000000000;
+  Near.call([
+    {
+      contractName: daoId,
+      methodName: "add_proposal",
+      args: {
+        proposal: {
+          description: "Remove member",
+          kind: {
+            RemoveMemberFromRole: {
+              member_id: state.member_id ?? accountId,
+              role: state.role ?? "council"
+            }
+          }
         }
-    ]);
+      },
+      gas: gas,
+      deposit: deposit
+    }
+  ]);
 };
 
 const onChangeMember = (member_id) => {
-    State.update({
-        member_id,
-        error: undefined
-    });
+  State.update({
+    member_id,
+    error: undefined
+  });
 };
 
 const onChangeRole = (role) => {
-    State.update({
-        role,
-        error: undefined
-    });
+  State.update({
+    role,
+    error: undefined
+  });
 };
 
 return (
-    <>
-        <div className="mb-3">
-            <h5>Account ID</h5>
-            <input
-                type="text"
-                onChange={(e) => onChangeMember(e.target.value)}
-            />
-        </div>
-        <div className="mb-3">
-            <Widget
-                src={`sking.near/widget/Common.Inputs.Select`}
-                props={{
-                    label: "Role",
-                    noLabel: false,
-                    placeholder: "Select the role",
-                    options: state.rolesOptions,
-                    value: { text: state.role, value: state.role },
-                    onChange: (role) => {
-                        onChangeRole(role.value);
-                    },
-                    error: undefined
-                }}
-            />
-        </div>
+  <>
+    <div className="mb-3">
+      <h5>Account ID</h5>
+      <input type="text" onChange={(e) => onChangeMember(e.target.value)} />
+    </div>
+    <div className="mb-3">
+      <Widget
+        src={`sking.near/widget/Common.Inputs.Select`}
+        props={{
+          label: "Role",
+          noLabel: false,
+          placeholder: "Select the role",
+          options: state.rolesOptions,
+          value: { text: state.role, value: state.role },
+          onChange: (role) => {
+            onChangeRole(role.value);
+          },
+          error: undefined
+        }}
+      />
+    </div>
 
-        {state.error && <div className="text-danger">{state.error}</div>}
-        <div className="ms-auto">
-            <Widget
-                src="sking.near/widget/Common.Button"
-                props={{
-                    children: "Propose To Remove Member",
-                    onClick: handleProposal,
-                    className: "mt-2",
-                    variant: "success"
-                }}
-            />
-            {onClose && (
-                <Widget
-                    src="sking.near/widget/Common.Button"
-                    props={{
-                        children: "Close",
-                        onClick: onClose,
-                        className: "mt-2"
-                    }}
-                />
-            )}
-        </div>
-    </>
+    {state.error && <div className="text-danger">{state.error}</div>}
+    <div className="ms-auto">
+      <Widget
+        src="sking.near/widget/Common.Button"
+        props={{
+          children: "Propose To Remove Member",
+          onClick: handleProposal,
+          className: "mt-2",
+          variant: "success"
+        }}
+      />
+      {onClose && (
+        <Widget
+          src="sking.near/widget/Common.Button"
+          props={{
+            children: "Close",
+            onClick: onClose,
+            className: "mt-2"
+          }}
+        />
+      )}
+    </div>
+  </>
 );
--- apps/astraplusplus/widget/DAO/Proposal/Create/Text.jsx
+++ apps/astraplusplus/widget/DAO/Proposal/Create/Text.jsx
@@ -6,159 +6,160 @@ const isCongressDaoID = props.isCongressDaoID;
 const isVotingBodyDao = props.isVotingBodyDao;
 const registry = props.registry;
 const kind = props.kind;
+const policy = props.daoPolicy;
 
 const HoMDaoId = props.dev
-    ? "/*__@replace:HoMDaoIdTesting__*/"
-    : "/*__@replace:HoMDaoId__*/";
+  ? "/*__@replace:HoMDaoIdTesting__*/"
+  : "/*__@replace:HoMDaoId__*/";
 
 if (!accountId) {
-    return "Please connect your NEAR wallet :)";
+  return "Please connect your NEAR wallet :)";
 }
 
 State.init({
-    description: state.description,
-    error: state.error,
-    attachDeposit: 0,
-    proposalQueue: null
+  description: state.description,
+  error: state.error,
+  attachDeposit: 0,
+  proposalQueue: null
 });
 
 const handleProposal = () => {
-    if (!state.description) {
-        State.update({
-            error: "Please enter a description"
-        });
-        return;
-    }
-    const gas = 200000000000000;
-    const deposit = state.attachDeposit
-        ? Big(state.attachDeposit)
-        : 100000000000000000000000;
-    const args = isCongressDaoID
-        ? {
-              description: state.description,
-              kind: "Text"
-          }
-        : {
-              proposal: {
-                  description: state.description,
-                  kind: "Vote"
-              }
-          };
-    if (isVotingBodyDao) {
-        const args = JSON.stringify({
-            description: state.description,
-            kind: kind,
-            caller: accountId
-        });
-
-        if (!state.proposalQueue) {
-            State.update({
-                error: "Please select proposal queue"
-            });
-            return;
+  if (!state.description) {
+    State.update({
+      error: "Please enter a description"
+    });
+    return;
+  }
+  const gas = 200000000000000;
+  const deposit = state.attachDeposit
+    ? Big(state.attachDeposit)
+    : 100000000000000000000000;
+  const args = isCongressDaoID
+    ? {
+        description: state.description,
+        kind: "Text"
+      }
+    : {
+        proposal: {
+          description: state.description,
+          kind: "Vote"
         }
+      };
+  if (isVotingBodyDao) {
+    const args = JSON.stringify({
+      description: state.description,
+      kind: kind,
+      caller: accountId
+    });
 
-        Near.call([
-            {
-                contractName: registry,
-                methodName: "is_human_call",
-                args: {
-                    ctr: daoId,
-                    function: "create_proposal",
-                    payload: args
-                },
-                gas: gas,
-                deposit: deposit
-            }
-        ]);
-    } else {
-        Near.call([
-            {
-                contractName: daoId,
-                methodName:
-                    isCongressDaoID || isVotingBodyDao
-                        ? "create_proposal"
-                        : "add_proposal",
-                args: args,
-                gas: gas,
-                deposit: deposit
-            }
-        ]);
+    if (!state.proposalQueue) {
+      State.update({
+        error: "Please select proposal queue"
+      });
+      return;
     }
+
+    Near.call([
+      {
+        contractName: registry,
+        methodName: "is_human_call",
+        args: {
+          ctr: daoId,
+          function: "create_proposal",
+          payload: args
+        },
+        gas: gas,
+        deposit: deposit
+      }
+    ]);
+  } else {
+    Near.call([
+      {
+        contractName: daoId,
+        methodName:
+          isCongressDaoID || isVotingBodyDao
+            ? "create_proposal"
+            : "add_proposal",
+        args: args,
+        gas: gas,
+        deposit: policy?.proposal_bond || 100000000000000000000000
+      }
+    ]);
+  }
 };
 
 const onChangeDescription = (description) => {
-    State.update({
-        description,
-        error: undefined
-    });
+  State.update({
+    description,
+    error: undefined
+  });
 };
 
 const onChangeQueue = ({ amount, queue }) => {
-    State.update({
-        attachDeposit: amount,
-        proposalQueue: queue
-    });
+  State.update({
+    attachDeposit: amount,
+    proposalQueue: queue
+  });
 };
 
 const defaultDescription =
-    "### [Your Title Here]\n\n#### Description\n\n[Detailed description of what the proposal is about.]\n\n#### Why This Proposal?\n\n[Explanation of why this proposal is necessary or beneficial.]\n\n#### Execution Plan\n\n[Description of how the proposal will be implemented.]\n\n#### Budget\n\n[If applicable, outline the budget required to execute this proposal.]\n\n#### Timeline\n\n[Proposed timeline for the execution of the proposal.]";
+  "### [Your Title Here]\n\n#### Description\n\n[Detailed description of what the proposal is about.]\n\n#### Why This Proposal?\n\n[Explanation of why this proposal is necessary or beneficial.]\n\n#### Execution Plan\n\n[Description of how the proposal will be implemented.]\n\n#### Budget\n\n[If applicable, outline the budget required to execute this proposal.]\n\n#### Timeline\n\n[Proposed timeline for the execution of the proposal.]";
 
 return (
-    <>
-        {isVotingBodyDao && (
-            <div className="mb-3">
-                This proposal requires
-                <a
-                    href={`https://github.com/near-ndc/voting-v1/blob/master/voting_body/README.md?#quorums-and-thresholds`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: "rgb(68, 152, 224)" }}
-                >
-                    Near {kind === "TextSuper" && " Super"} Consent.
-                </a>
-            </div>
-        )}
+  <>
+    {isVotingBodyDao && (
+      <div className="mb-3">
+        This proposal requires
+        <a
+          href={`https://github.com/near-ndc/voting-v1/blob/master/voting_body/README.md?#quorums-and-thresholds`}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "rgb(68, 152, 224)" }}
+        >
+          Near {kind === "TextSuper" && " Super"} Consent.
+        </a>
+      </div>
+    )}
+    <Widget
+      src="/*__@appAccount__*//widget/DAO.Proposal.Common.ProposalQueue"
+      props={{
+        daoId: daoId,
+        onUpdate: onChangeQueue,
+        dev: props.dev
+      }}
+    />
+    <div className="mb-3">
+      <h5>Proposal Description</h5>
+      <Widget
+        src="sking.near/widget/Common.Inputs.Markdown"
+        props={{
+          onChange: (value) => onChangeDescription(value),
+          height: "270px",
+          initialText: defaultDescription
+        }}
+      />
+    </div>
+    {state.error && <div className="text-danger">{state.error}</div>}
+    <div className="ms-auto">
+      <Widget
+        src="sking.near/widget/Common.Button"
+        props={{
+          children: "Create Proposal",
+          onClick: handleProposal,
+          className: "mt-2",
+          variant: "success"
+        }}
+      />
+      {onClose && (
         <Widget
-            src="/*__@appAccount__*//widget/DAO.Proposal.Common.ProposalQueue"
-            props={{
-                daoId: daoId,
-                onUpdate: onChangeQueue,
-                dev: props.dev
-            }}
+          src="sking.near/widget/Common.Button"
+          props={{
+            children: "Close",
+            onClick: onClose,
+            className: "mt-2"
+          }}
         />
-        <div className="mb-3">
-            <h5>Proposal Description</h5>
-            <Widget
-                src="sking.near/widget/Common.Inputs.Markdown"
-                props={{
-                    onChange: (value) => onChangeDescription(value),
-                    height: "270px",
-                    initialText: defaultDescription
-                }}
-            />
-        </div>
-        {state.error && <div className="text-danger">{state.error}</div>}
-        <div className="ms-auto">
-            <Widget
-                src="sking.near/widget/Common.Button"
-                props={{
-                    children: "Create Proposal",
-                    onClick: handleProposal,
-                    className: "mt-2",
-                    variant: "success"
-                }}
-            />
-            {onClose && (
-                <Widget
-                    src="sking.near/widget/Common.Button"
-                    props={{
-                        children: "Close",
-                        onClick: onClose,
-                        className: "mt-2"
-                    }}
-                />
-            )}
-        </div>
-    </>
+      )}
+    </div>
+  </>
 );
