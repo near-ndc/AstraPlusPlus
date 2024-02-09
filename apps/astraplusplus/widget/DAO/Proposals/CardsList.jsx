0497d7150c001dd072cb0b8cd362166071f80f46
Megha-Dev-19 <meghadev1901@gmail.com>
2024-02-09 17:07:04 +0530

    fix daoID

--- apps/astraplusplus/widget/DAO/Proposals/CardsList.jsx
+++ apps/astraplusplus/widget/DAO/Proposals/CardsList.jsx
@@ -1,58 +1,56 @@
 const {
-    proposals,
-    resPerPage,
-    state,
-    update,
-    isCongressDaoID,
-    daoConfig,
-    isVotingBodyDao
+  proposals,
+  resPerPage,
+  state,
+  update,
+  isCongressDaoID,
+  daoConfig,
+  isVotingBodyDao
 } = props;
 
 return (
-    <div>
-        {proposals === null && state.multiSelectMode && (
-            <>
-                {new Array(resPerPage).fill(0).map((_, i) => (
-                    <Widget src="astraplusplus.ndctools.near/widget/DAO.Proposals.Card.skeleton" />
-                ))}
-            </>
-        )}
-        {proposals !== null &&
-            proposals.map(({ proposal, proposal_type, proposal_id }, i) => {
-                if (!isCongressDaoID && !isVotingBodyDao) {
-                    proposal.kind = {
-                        [proposal_type]: {
-                            ...proposal.kind
-                        }
-                    };
-                }
+  <div>
+    {proposals === null && state.multiSelectMode && (
+      <>
+        {new Array(resPerPage).fill(0).map((_, i) => (
+          <Widget src="astraplusplus.ndctools.near/widget/DAO.Proposals.Card.skeleton" />
+        ))}
+      </>
+    )}
+    {proposals !== null &&
+      proposals.map(({ proposal, proposal_type, proposal_id, dao_id }, i) => {
+        if (!isCongressDaoID && !isVotingBodyDao) {
+          proposal.kind = {
+            [proposal_type]: {
+              ...proposal.kind
+            }
+          };
+        }
 
-                proposal.id = proposal_id;
-                if (proposal.status === "Removed") return <></>;
-                Object.keys(proposal.vote_counts).forEach((k) => {
-                    if (typeof proposal.vote_counts[k] == "string") {
-                        proposal.vote_counts[k] = proposal.vote_counts[k]
-                            .match(/.{1,2}/g)
-                            .map((x) => parseInt(x));
-                    }
-                });
-                return (
-                    <Widget
-                        key={i}
-                        src={
-                            "astraplusplus.ndctools.near/widget/DAO.Proposals.Card.index"
-                        }
-                        props={{
-                            daoId: state.daoId,
-                            proposalString: JSON.stringify(proposal),
-                            multiSelectMode: state.multiSelectMode,
-                            isCongressDaoID,
-                            isVotingBodyDao,
-                            daoConfig,
-                            dev: props.dev
-                        }}
-                    />
-                );
-            })}
-    </div>
+        proposal.id = proposal_id;
+        if (proposal.status === "Removed") return <></>;
+        Object.keys(proposal.vote_counts).forEach((k) => {
+          if (typeof proposal.vote_counts[k] == "string") {
+            proposal.vote_counts[k] = proposal.vote_counts[k]
+              .match(/.{1,2}/g)
+              .map((x) => parseInt(x));
+          }
+        });
+        return (
+          <Widget
+            key={i}
+            src={"astraplusplus.ndctools.near/widget/DAO.Proposals.Card.index"}
+            props={{
+              daoId: state.daoId ?? dao_id,
+              proposalString: JSON.stringify(proposal),
+              multiSelectMode: state.multiSelectMode,
+              isCongressDaoID,
+              isVotingBodyDao,
+              daoConfig,
+              dev: props.dev
+            }}
+          />
+        );
+      })}
+  </div>
 );
