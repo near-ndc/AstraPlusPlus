3bacf8afb6e1de6ad9995c4fab41ab707850a070
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-04-16 17:28:48 +0530

    added mobile responsive fixes for proposals page (#107)

--- apps/astraplusplus/widget/Common/Modals/Comments.jsx
+++ apps/astraplusplus/widget/Common/Modals/Comments.jsx
@@ -2,48 +2,48 @@ const commentsCount = props.commentsCount;
 const item = props.item;
 
 const Wrapper = styled.div`
-    padding: 24px;
-    border-radius: 14px;
+  padding: 24px;
+  border-radius: 14px;
 
-    & > div {
-        margin-left: 0 !important;
-    }
-    textarea {
-        font-size: 0.9rem !important;
-        margin-bottom: 24px !important;
-    }
+  & > div {
+    margin-left: 0 !important;
+  }
+  textarea {
+    font-size: 0.9rem !important;
+    margin-bottom: 24px !important;
+  }
 
-    p {
-        margin-bottom: 16px;
-    }
+  p {
+    margin-bottom: 16px;
+  }
 `;
 
 const Hr = styled.hr`
-    border: none;
-    border-top: 1px solid #d7dbdf;
-    margin: 16px 0;
+  border: none;
+  border-top: 1px solid #d7dbdf;
+  margin: 16px 0;
 `;
 
 return (
-    <Wrapper className="mb-2 w-100 ndc-card">
-        <Widget
-            src={`mob.near/widget/MainPage.N.Comment.Compose`}
-            props={{
-                item,
-                onComment: () => State.update({ showReply: false })
-            }}
-        />
-        <Widget
-            src="mob.near/widget/MainPage.N.Comment.Feed"
-            props={{
-                item,
-                subscribe: true
-            }}
-        />
-        {commentsCount === 0 && (
-            <span className="text-muted text-center mt-3">
-                No comments yet. Be the first to comment!
-            </span>
-        )}
-    </Wrapper>
+  <Wrapper className="mb-2 w-100">
+    <Widget
+      src={`mob.near/widget/MainPage.N.Comment.Compose`}
+      props={{
+        item,
+        onComment: () => State.update({ showReply: false })
+      }}
+    />
+    <Widget
+      src="mob.near/widget/MainPage.N.Comment.Feed"
+      props={{
+        item,
+        subscribe: true
+      }}
+    />
+    {commentsCount === 0 && (
+      <span className="text-muted text-center mt-3">
+        No comments yet. Be the first to comment!
+      </span>
+    )}
+  </Wrapper>
 );
--- apps/astraplusplus/widget/Common/Modals/Voters.jsx
+++ apps/astraplusplus/widget/Common/Modals/Voters.jsx
@@ -15,159 +15,153 @@ const query = `query MyQuery {
   }`;
 
 function fetchGraphQL(operationsDoc, operationName, variables) {
-    return asyncFetch(QUERYAPI_ENDPOINT, {
-        method: "POST",
-        headers: { "x-hasura-role": "strachu_near" },
-        body: JSON.stringify({
-            query: operationsDoc,
-            variables: variables,
-            operationName: operationName
-        })
-    });
+  return asyncFetch(QUERYAPI_ENDPOINT, {
+    method: "POST",
+    headers: { "x-hasura-role": "strachu_near" },
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables: variables,
+      operationName: operationName
+    })
+  });
 }
-if (isVotingBodyDao) {
-    votes = useCache(
-        () =>
-            fetchGraphQL(query, "MyQuery", {}).then((result) => {
-                const parsedData = {};
-                if (result.status === 200) {
-                    if (result.body.data) {
-                        const data =
-                            result.body.data["strachu_near_vb_prod_v2_vote"];
 
-                        if (data?.length > 0) {
-                            for (const a of data) {
-                                parsedData[a.voter] = {
-                                    timestamp: a.timestamp,
-                                    vote: a.vote
-                                };
-                            }
-                        }
-                    }
-                }
-                return parsedData;
-            }),
-        daoId + "proposals" + proposalId,
-        { subscribe: false }
-    );
+if (isVotingBodyDao) {
+  votes = useCache(
+    () =>
+      fetchGraphQL(query, "MyQuery", {}).then((result) => {
+        const parsedData = {};
+        if (result.status === 200) {
+          if (result.body.data) {
+            const data = result.body.data["strachu_near_vb_prod_v2_vote"];
+
+            if (data?.length > 0) {
+              for (const a of data) {
+                parsedData[a.voter] = {
+                  timestamp: a.timestamp,
+                  vote: a.vote
+                };
+              }
+            }
+          }
+        }
+        return parsedData;
+      }),
+    daoId + "proposals" + proposalId,
+    { subscribe: false }
+  );
 }
 
 const Wrapper = styled.ul`
-    background: #f8f9fa;
+  overflow: hidden;
+  padding: 24px;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  .radius {
     border-radius: 14px;
-    overflow: hidden;
-    padding: 24px;
-    list-style: none;
+  }
+
+  li {
     display: flex;
     flex-direction: column;
-    gap: 12px;
+    padding: 12px;
+    gap: 10px;
+    border-radius: 14px;
+  }
 
-    .radius {
-        border-radius: 14px;
+  div.Approve {
+    background-color: #59e69220;
+    .vote {
+      color: #0d562b;
     }
-
-    li {
-        display: flex;
-        flex-direction: column;
-        padding: 12px;
-        gap: 10px;
-        border-radius: 14px;
+  }
+  div.Reject {
+    background-color: #e5484d20;
+    .vote {
+      color: #bf2c30;
     }
-
-    div.Approve {
-        background-color: #59e69220;
-        .vote {
-            color: #0d562b;
-        }
+  }
+  div.Abstain {
+    background-color: #d3d3d3;
+    .vote {
+      color: black;
     }
-    div.Reject {
-        background-color: #e5484d20;
-        .vote {
-            color: #bf2c30;
-        }
+  }
+  div.Spam {
+    background-color: #ffda0920;
+    .vote {
+      color: #73692d;
     }
-    div.Abstain {
-        background-color: #d3d3d3;
-        .vote {
-            color: black;
-        }
+  }
+  div.Remove {
+    background-color: #ffda0920;
+    .vote {
+      color: #73692d;
     }
-    div.Spam {
-        background-color: #ffda0920;
-        .vote {
-            color: #73692d;
-        }
-    }
-    div.Remove {
-        background-color: #ffda0920;
-        .vote {
-            color: #73692d;
-        }
-    }
+  }
 `;
 
 return (
-    <Wrapper>
-        {Object.keys(votes).length === 0 && (
-            <span className="text-muted text-center">No votes yet</span>
-        )}
-        {isCongressDaoID &&
-            isVotingBodyDao &&
-            votersCount > Object.keys(votes).length && (
-                <div>
-                    Note: Indexer is currently running behind schedule, and the
-                    expected results may take longer to appear. We appreciate
-                    your patience and apologize for any inconvenience.
+  <Wrapper>
+    {Object.keys(votes).length === 0 && (
+      <span className="text-muted text-center">No votes yet</span>
+    )}
+    {isCongressDaoID &&
+      isVotingBodyDao &&
+      votersCount > Object.keys(votes).length && (
+        <div>
+          Note: Indexer is currently running behind schedule, and the expected
+          results may take longer to appear. We appreciate your patience and
+          apologize for any inconvenience.
+        </div>
+      )}
+    {Object.keys(votes).map((voterId) => {
+      const vote =
+        isCongressDaoID || isVotingBodyDao
+          ? votes[voterId].vote
+          : votes[voterId];
+      return (
+        <div className={vote + " radius"}>
+          <li className={vote}>
+            <div className="d-flex items-center justify-content-between">
+              <div className="text-truncate w-70">
+                <Widget
+                  src="mob.near/widget/Profile.ShortInlineBlock"
+                  props={{
+                    accountId: voterId,
+                    tooltip: true
+                  }}
+                />
+              </div>
+              <div className="w-30">
+                <Widget
+                  src="/*__@replace:nui__*//widget/Element.Badge"
+                  props={{
+                    children: <span className="vote">{vote}</span>,
+                    variant: `round`,
+                    size: "md"
+                  }}
+                />
+              </div>
+            </div>
+            {((isCongressDaoID && votes[voterId].timestamp > 0) ||
+              isVotingBodyDao) &&
+              votes[voterId].timestamp && (
+                <div className="d-flex gap-2">
+                  <small>
+                    <i className="bi bi-clock" />
+                  </small>
+                  <small className="text-secondary">
+                    {new Date(votes[voterId].timestamp).toLocaleString()}
+                  </small>
                 </div>
-            )}
-        {Object.keys(votes).map((voterId) => {
-            const vote =
-                isCongressDaoID || isVotingBodyDao
-                    ? votes[voterId].vote
-                    : votes[voterId];
-            return (
-                <div className={vote + " radius"}>
-                    <li className={vote}>
-                        <div className="d-flex items-center justify-content-between">
-                            <div className="text-truncate w-70">
-                                <Widget
-                                    src="mob.near/widget/Profile.ShortInlineBlock"
-                                    props={{
-                                        accountId: voterId,
-                                        tooltip: true
-                                    }}
-                                />
-                            </div>
-                            <div className="w-30">
-                                <Widget
-                                    src="/*__@replace:nui__*//widget/Element.Badge"
-                                    props={{
-                                        children: (
-                                            <span className="vote">{vote}</span>
-                                        ),
-                                        variant: `round`,
-                                        size: "md"
-                                    }}
-                                />
-                            </div>
-                        </div>
-                        {((isCongressDaoID && votes[voterId].timestamp > 0) ||
-                            isVotingBodyDao) &&
-                            votes[voterId].timestamp && (
-                                <div className="d-flex gap-2">
-                                    <small>
-                                        <i className="bi bi-clock" />
-                                    </small>
-                                    <small className="text-secondary">
-                                        {new Date(
-                                            votes[voterId].timestamp
-                                        ).toLocaleString()}
-                                    </small>
-                                </div>
-                            )}
-                    </li>
-                </div>
-            );
-        })}
-    </Wrapper>
+              )}
+          </li>
+        </div>
+      );
+    })}
+  </Wrapper>
 );
